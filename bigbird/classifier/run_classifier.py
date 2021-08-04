7ef5422c57a46d316e6da8ac126d00039458f635
Yuqi Si <ys2887@columbia.edu>
2021-08-04 14:12:17 -0500

    string error, fixed

--- bigbird/classifier/run_classifier.py
+++ bigbird/classifier/run_classifier.py
@@ -461,8 +461,10 @@ def main(_):
     ]
     all_ckpts = natsorted(all_ckpts)
     # only eval last ckpt for test as of now
+    last_ckpt = []
+    last_ckpt.append(all_ckpts[-1])
     logging.info("!!!!!!!!!!!!updated version!!!!!!!!!!!!!!")
-    for ckpt in all_ckpts[-1]:
+    for ckpt in last_ckpt:
       current_step = int(os.path.basename(ckpt).split("-")[1])
       output_eval_file = os.path.join(
           FLAGS.output_dir, "eval_results_{}.txt".format(current_step))
